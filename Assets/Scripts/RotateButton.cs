4063515461e8c0501d9a5289bfd7b308b17c2a9c
ScarredSceptile <jonas.haegd@gmail.com>
2020-06-27 12:25:09 +0200

    finish project

---
+++ Assets/Scripts/RotateButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateButton : MonoBehaviour
+{
+    public Rubiks.RubiksColors color;
+    public bool clockwise;
+    public Rubiks cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => cube.Rotate(color, clockwise));
+    }
+}
